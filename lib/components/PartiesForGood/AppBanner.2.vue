665f7179fdb39adbba2acb7d1a099a02ab9d108c
John Vee Uminga <johnveeuminga@example.com>
2019-05-16 02:28:07 +0800

    Added AppBanner v2 for Parties for Good.

---
+++ lib/components/PartiesForGood/AppBanner.2.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="banner">
+    <flickity
+      class='banner__slider'
+      ref='mainSlder'
+      :options='sliderOptions'
+      @init='onMainSliderReady'
+    >
+      <div 
+        class="banner__slide" 
+        v-for='(slide, index) in slides'
+        :key='index'
+      >
+        <img 
+          :src="require(`@/assets/img/images/${slide.img}`)"
+          :alt="`Banner Image ${index}`"
+          :class='`banner__img banner__img--${index}`'
+        >
+      </div>
+    </flickity>
+    <flickity
+      class='banner__captions-slider'
+      ref='captionSlider'
+      :options='{
+        ...sliderOptions,
+        pageDots: false,
+        prevNextButtons: false,
+        autoPlay: false,
+        draggable: false,
+      }'
+      @init='onCaptionSliderReady'
+    >
+      <div 
+        class="banner__captions-slide"
+        v-for='(slide, index) in slides'
+        :key='index'
+      >
+        <h2 class='is-marginless has-text-centered'>{{ slide.caption }} </h2>
+      </div>
+    </flickity>
+  </div>
+</template>
+
+<script>
+import Flickity from 'Components/plugins/Flickity'
+
+export default {
+  name: 'AppBanner',
+
+  components: {
+    Flickity,
+  },
+
+  methods: {
+    onMainSliderReady (slider) {
+      slider.on('change', (index) => {
+        this.$refs.captionSlider.select(index)
+      })
+    },
+
+    onCaptionSliderReady (slider) {
+      slider.on('change', (index) => {
+        this.$refs.mainSlider.select(index)
+      })
+    }
+  }, 
+
+  data () {
+    return {
+      slides: [
+        {
+          img: 'banner-img-1.jpg',
+          caption: 'Classic cookout party',
+        },
+        {
+          img: 'banner-img-4.jpg',
+          caption: '80s outfits party',
+        },
+        {
+          img: 'banner-img-5.jpg',
+          caption: 'Back to the 60s party',
+        },
+        {
+          img: 'banner-img-6.jpg',
+          caption: '90s costume party',
+        },
+        {
+          img: 'banner-img-7.jpg',
+          caption: 'Family pool party',
+        },
+        {
+          img: 'banner-img-8.jpg',
+          caption: 'Grown-ups pool party',
+        },
+        {
+          img: 'banner-img-9.jpg',
+          caption: 'Sunset Beach Party',
+        },
+        {
+          img: 'banner-img-10.jpg',
+          caption: 'Beach party',
+        },
+        {
+          img: 'banner-img-11.jpg',
+          caption: 'Home karaoke party',
+        },
+      ],
+      sliderOptions: {
+        cellAlign: 'left',
+        autoPlay: 4000,
+        wrapAround: true,
+      },
+    }
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+.banner {
+  position: relative;
+
+  &__slider {
+    height: 600px;
+  }
+
+  &__slide {
+    width: 100%;
+    height: 100%;
+  }
+
+  &__img {
+    width: 100%;
+    height: 600px;
+    object-fit: cover;
+  }
+
+  &__captions-slider {
+    height: 40px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    background-color: rgba(0,0,0,.4);
+  }
+
+  &__captions-slide {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h2 {
+      font-size: 24px;
+      color: #fff;
+    }
+  }
+
+}
+</style>
+
+<style lang='scss'>
+.banner {
+  .flickity-page-dots {
+    bottom: 45px;
+  }
+
+  .flickity-button,
+  .flickity-page-dots {
+    opacity: 0;
+    transition: opacity .2s ease;
+  }
+
+  &:hover {
+    .flickity-button,
+    .flickity-page-dots {
+      opacity: 1;
+    }
+  }
+}
+</style>
