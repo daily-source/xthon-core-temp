1413d9d4352e3e22b6843f4cd02dede013acbe06
John Vee Uminga <johnveeuminga@example.com>
2019-05-26 15:59:32 +0800

    Added Loseathon components.

---
+++ lib/components/Loseathon/AppBanner.vue
@@ -0,0 +1,26 @@
+<template>
+<div class='banner'>
+  <img src='@/assets/img/loseathon-banner.jpg' alt=''>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'AppBanner',
+}
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  height: 550px;
+  background-color: #fff;
+  
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    object-position: right;
+  }
+}
+</style>
+
