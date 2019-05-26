d45ae7a9c751b8b1fd56611ff3c4d590e3794446
John Vee Uminga <johnveeuminga@example.com>
2019-05-26 17:32:58 +0800

    Added WalkForGood components.

---
+++ lib/components/WalkForGood/AppHeader.vue
@@ -0,0 +1,355 @@
+<template>
+  <div class="header__wrapper white-bg" :class="{ 'page-layout': layout === 'page' }">
+    <div class="container is-fluid">
+      <div class="header__inner">
+        <div class="random-words">
+          <router-link to="/explore">ExploreXY</router-link>
+        </div>
+        <div class="random-words">
+          <router-link to='#'>Do One Now</router-link>
+        </div>
+        <div class="user-space__search">
+          <div class="user-space__search-wrapper">
+            <div class="user-space__search-trigger" @click="showSearchBar = !showSearchBar">
+              <Icons iconwidth="24px" iconheight="24px" icon="search" color="#999999" class=""/>
+            </div>
+          </div>
+        </div>
+        <div class="logo__wrapper">
+          <router-link to="/" exact>
+            <img class="logo" :src="require('Public/walk-for-good-logo-300x73.png')" alt="logo">
+          </router-link>
+        </div>
+        <div class="logo__wrapper centered">
+          <router-link to="/" exact>
+            <img class="logo" :src="require('Public/walk-for-good-logo-300x73.png')" alt="logo">
+          </router-link>
+        </div>
+        <div class="user-space__logged--in modal-trigger" :class="{'login-highlight': loggedIn}">
+          <LogInModal
+            layout="app-header"
+            :register="false"
+          ></LogInModal>
+        </div>
+        <div class="max-good" @click="toggleOtherMaxGoodSites()" :class="{ 'active': showingMaxGoodSites }">
+          <span class="small">Powered by </span>
+          <div class="max-good__submenu-wrapper">
+            <img :src="require('Public/max-good-logo-150x18.png')" width="150" height="18" alt="">
+            <ul class="other-max-good-sites">
+              <li>
+                <span class="other-sites-arrow" :class="{ 'turn': showingMaxGoodSites }">
+                  <Icons iconwidth="24px" iconheight="24px" icon="chevron-down" color="#999999" />
+                </span>
+                <ul class="sub" :class="{ 'active': showingMaxGoodSites }">
+                  <li v-for="site in maxGoodSites"><a :href="site.href" target="_blank">{{ site.name }}</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.other-max-good-sites {
+  padding-left: 10px;
+  margin-top: -5px;
+
+  li {
+    position: relative;
+
+    .other-sites-arrow {
+      font-size: 36px;
+      line-height: 18px;
+      color: $color-medium-gray;
+      svg {
+        transition: transform 0.2s ease-in-out;
+        transform: rotate(0deg);
+      }
+
+      &.turn {
+        svg {
+          @include mobile {
+            transition: transform 0.2s ease-in-out;
+            transform: rotate(180deg);
+          }
+        }
+      }
+    }
+
+    ul.sub {
+      opacity: 0;
+      z-index: -1;
+      transition: opacity 0.2s ease-in-out;
+      position: absolute;
+      top: 35px;
+      right: 0;
+      margin-top: -10px;
+      background: rgba($white, 0.9);
+      text-align: right;
+      border: 1px solid $color-light-gray;
+
+      @include widescreen {
+        top: 30px;
+      }
+
+      &.active {
+        opacity: 1;
+        z-index: 24;
+
+        @include widescreen {
+          opacity: 0;
+          z-index: -1;
+        }
+      }
+
+      li {
+        border-bottom: 1px solid $color-light-gray;
+        min-width: 65vw;
+        transition: background-color 0.2s ease-in-out;
+
+        @include desktop {
+          min-width: 40vw;
+        }
+
+        @include widescreen {
+          min-width: 300px;
+        }
+
+        a {
+          display: block;
+          padding: 10px 30px;
+          transform: translateX(20px);
+        }
+
+        &:hover {
+          background: rgba($color-light-gray, 0.8);
+        }
+      }
+    }
+  }
+}
+
+.max-good {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 0;
+  width: 100%;
+  max-height: 50px;
+  overflow: hidden;
+  z-index: 30;
+
+  &.active {
+    max-height: auto;
+    overflow: visible;    
+  }
+
+  @include tablet {
+    width: auto;
+  }
+  .small {
+    padding-right: 10px;
+    font-size: 13px;
+    align-self: flex-end;
+  }
+
+  &__submenu-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+  }
+
+  @include desktop {
+    flex-direction: column;
+    &:hover {
+      max-height: auto;
+      overflow: visible;
+
+      .other-sites-arrow {
+        color: $color-light-gray;
+        svg {
+          transition: transform 0.2s ease-in-out;
+          transform: rotate(180deg);
+        }
+      }
+
+      ul.sub {
+        opacity: 1;
+        z-index: 24;
+
+        &.active {
+          opacity: 1;
+          z-index: 24;
+        }
+      }
+    }
+  }
+}
+
+.header__wrapper {
+  border-bottom: 2px solid $color-emphasis-alt;
+}
+
+.header__inner {
+  display: flex;
+  text-align: center;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 5px 0;
+  align-items: center;
+  position: relative;
+  font-size: 16px;
+
+  @include desktop {
+    flex-direction: row;
+  }
+
+  div {
+    z-index: 10;    
+    &.modal-trigger {
+      z-index: 11;
+    }
+  }
+
+  .user-space__search {
+    order: -3;
+  }
+
+  .logo__wrapper {
+    width: 100%;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    order: -100;
+    opacity: 1;
+    z-index: 1;
+
+    @include mobile {
+      padding-left: 9px;
+    }
+
+    @include desktop {
+      width: auto;
+      order: unset;
+      opacity: 0;
+
+      img {
+        max-width: 272px;
+        width: 250px;
+        height: auto;    
+      }
+    }
+
+    &.centered {
+      position: absolute;
+      margin: 0 auto;
+      opacity: 0;
+
+      @include desktop {
+        width: 100%;
+        order: unset;
+        padding-top: 5px;
+        opacity: 1;
+      }
+    }
+  }
+}
+
+.random-words {
+  padding: 0 5px;
+}
+
+.page-layout {
+  .header__inner {
+    .user-space__search {
+      order: 2;
+    }
+    .max-good {
+      order: 3;
+    }
+
+    .logo__wrapper {
+      opacity: 1;
+      order: -2;
+
+      img {
+        max-width: 272px;
+        max-height: 49px;        
+      }
+
+      @include tablet {
+        img {
+          max-width: 300px;
+          max-height: 53px;        
+        }        
+      }
+
+      &.centered {
+        opacity: 0;
+        display: none;
+      }
+    }
+  }
+}
+
+.login-highlight {
+  .trigger {
+    padding: 5px;
+    animation: fadeHighlight 7s ease-in-out;
+  }
+}
+
+</style>
+
+<script>
+import Icons from "Components/general/Icons.vue"
+import LogInModal from "Components/general/LogInModal.vue"
+
+export default {
+  name: "AppHeader",
+  components: {
+    Icons,
+    LogInModal
+  },
+  props: ["layout"],
+  data () {
+    return {
+      showSearchBar: false,
+      showingMaxGoodSites: false,
+      maxGoodSites: [
+        { name: "Volunteerathon", href: "#" },
+        { name: "Quitathon", href: "#" },
+        { name: "Loseathon", href: "#" },
+        { name: "Give it up for Good", href: "#" },
+        { name: "Vacation for Good", href: "#" },
+        { name: "Waterathon", href: "#" },
+        { name: "Christmas for Good", href: "#" },
+        { name: "MLK Day for Good", href: "#" },
+        { name: "The Lent Site", href: "#" },
+        { name: "Valentines for Good", href: "#" },
+        { name: "Fools for Good", href: "#" },
+        { name: "Resolutions for Good", href: "#" },
+        { name: "Run for good", href: "#" },
+        { name: "Bike for Good", href: "#" },
+        { name: "Walk for Good", href: "#" },
+        { name: "Birthdays for Good", href: "#" },
+        { name: "Polar Plunge for Good", href: "#" },
+        { name: "Bake for Good", href: "#" }
+      ]
+    }
+  },
+  methods: {
+    toggleOtherMaxGoodSites () {
+      this.showingMaxGoodSites = !this.showingMaxGoodSites
+    }
+  },
+  computed: {
+    loggedIn () {
+      return this.$store.state.user.loggedIn
+    }
+  }
+}
+</script>
---
+++ lib/components/WalkForGood/NonprofitForm.vue
@@ -0,0 +1,308 @@
+<template>
+  <div class="page-wrapper">
+    <div class="container">
+      <slot name="heading"></slot>
+      <div class="columns combo-wrapper is-multiline bubbles-wrapper" v-if="bubbles">
+        <ComboBox :iconsrc="require('Public/img/check.png')" duplicate="false" :large="true">
+        </ComboBox>
+        <ComboBox :iconsrc="require('Public/img/invite.png')" duplicate="false" :large="true">
+        </ComboBox>
+        <ComboBox :iconsrc="require('Public/img/act.png')" duplicate="false" :large="true">
+        </ComboBox>
+      </div>
+      <div class="form-wrapper">
+        <form v-on:keyup.enter="">
+          <div class="columns form-column__wrapper  is-multiline">
+            <div class="column is-5 form-column__label-column">
+              <label :for="`input-email_${_uid}`" class="label">How many miles will you walk:</label>
+            </div>
+            <div class="column is-5 form-column__input-column">
+              <div class="control input-wrapper">
+                <Icons icon="chart" class="input-icon" iconwidth="20px" iconheight="20px" color="#999"></Icons>
+                <input class="input" type="number" name="action" placeholder=""
+                  :required="true"
+                  v-model="form.howManyMiles"
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="columns form-column__wrapper  is-multiline">
+            <div class="column is-5 form-column__label-column">
+              <label :for="`input-email_${_uid}`" class="label">Start location:</label>
+            </div>
+            <div class="column is-5 form-column__input-column">
+              <div class="control input-wrapper">
+                <Icons icon="location" class="input-icon" iconwidth="20px" iconheight="20px" color="#999"></Icons>
+                <input class="input" type="text" name="action" placeholder=""
+                  :required="true"
+                  v-model="form.startLocation"
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="columns form-column__wrapper  is-multiline">
+            <div class="column is-5 form-column__label-column">
+              <label :for="`input-email_${_uid}`" class="label">End location:</label>
+            </div>
+            <div class="column is-5 form-column__input-column">
+              <div class="control input-wrapper">
+                <Icons icon="location" class="input-icon" iconwidth="20px" iconheight="20px" color="#999"></Icons>
+                <input class="input" type="text" name="action" placeholder=""
+                  :required="true"
+                  v-model="form.endLocation"
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="columns form-column__wrapper  is-multiline">
+            <div class="column is-5 form-column__label-column">
+              <label :for="`input-email_${_uid}`" class="label">Date of the event:</label>
+            </div>
+            <div class="column is-5 form-column__input-column">
+              <div class="control input-wrapper">
+                <Icons icon="calendar" class="input-icon" iconwidth="20px" iconheight="20px" color="#999"></Icons>
+                <datetime v-model="form.date"></datetime>
+              </div>
+            </div>
+          </div>
+
+          <div class="columns form-column__wrapper  is-multiline" v-if="enableNonprofitSearch">
+            <div class="column is-5 form-column__label-column"><label class="label">Pick a nonprofit:</label></div>
+            <NonprofitAjaxSearch
+              v-if="canRender"
+              v-on:selected="setNonprofit($event)"
+              :standalone="false"
+            ></NonprofitAjaxSearch>
+          </div>
+
+          <div class="columns form-column__wrapper is-multiline">
+            <div class="column is-5 form-column__label-column"><label class="label">Is this:</label></div>
+            <div class="column is-5 form-column__label-column">
+              <label class="radio">
+                <input type="radio" name="non-profit-is" v-model="form.ownWalk" value="own">
+                Your own ride
+              </label>
+              <label class="radio">
+                <input type="radio" name="non-profit-is" v-model="form.ownWalk" value="event">
+                Part of an existing event
+              </label>
+            </div>
+          </div>
+
+
+          <transition name="slide-fade">
+            <div class="columns form-column__wrapper  is-multiline" v-if="form.ownWalk === 'event'">
+              <div class="column is-5 form-column__label-column">
+                <label :for="`input-email_${_uid}`" class="label">Name of the event</label>
+              </div>
+              <div class="column is-5 form-column__input-column">
+                <div class="control input-wrapper">
+                  <Icons icon="bike" class="input-icon" iconwidth="20px" iconheight="20px" color="#999"></Icons>
+                  <input class="input" type="text" name="action" placeholder=""
+                    :required="true"
+                    v-model="form.eventName"
+                  >
+                </div>
+              </div>
+            </div>
+          </transition>
+
+          <button
+            class="button is-success is-large"
+            type="submit"
+            @click.prevent="validateSubmit()"
+          >Continue</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icons from "Components/general/Icons.vue"
+
+export default {
+  props: ["submitButtonLabel", "enableNonprofitSearch", "bubbles"],
+  /**
+   * Uses dynamic import to speed up page performance.
+   * See https://webpack.js.org/guides/code-splitting/ for reference.
+   */
+  components: {
+    Icons,
+    ComboBox: () => import("Components/general/ComboBox.vue"),
+    NonprofitAjaxSearch: () => import("Components/general/NonprofitAjaxSearch.vue")
+  },
+  mounted () {
+    this.canRender = true
+    let storedForm = window.localStorage.getItem("nonprofitForm")
+    if (storedForm) {
+      this.form = JSON.parse(storedForm)
+    }
+  },
+  data () {
+    return {
+      canRender: false,
+      form: {},
+      errorMessage: "",
+      nonprofitErrorMessage: "",
+      nonprofitIsErrorMessage: "",
+      hoursErrorMessage: "",
+      targetNonprofitErrorMessage: ""
+    }
+  },
+  /**
+   * TODO: validate form, submit data to api, display thank you or error dialog.
+   */
+  methods: {
+    setNonprofit ($event) {
+      console.log($event)
+    }
+  },
+  computed: {
+    loggedIn () {
+      return this.$store.state.user.loggedIn
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.form-column {
+  &__wrapper {
+    margin-top: 0;
+    @include tablet {
+      line-height: 42px;
+    }
+
+    .column {
+      padding-top: 0;
+      padding-bottom: 0;
+      margin-bottom: 5px;
+      @include tablet {
+        margin-bottom: 0;
+      }
+    }
+  }
+  &__label-column {
+    text-align: left;
+    .label {
+    }
+  }
+  &__input-column {
+
+  }
+  &__button-column {
+    .button {
+      display: block;
+      width: 100%;
+      border-radius: 20px !important;
+    }
+  }
+  &__extra-padded {
+    margin-bottom: 35px;
+  }
+  &__left-padded {
+    padding-left: 80px;
+    padding-right: 0;
+    margin-right: 0;
+    margin-left: 0;
+  }
+}
+
+.inline-field {
+  display: inline-block;
+  margin: 0 5px 10px;
+  width: 65px;
+}
+
+.radio-fields {
+  display: block;
+}
+
+.page-wrapper {
+  background-color: $white;
+  color: #363636;
+  text-align: center;
+}
+
+h1 {
+  color: $color-emphasis;
+  margin-bottom: 40px;
+}
+
+.combo-wrapper {
+  justify-content: center;
+  max-width: $max-combo-wrapper-width;
+  margin: 0 auto;
+
+  @include tablet {
+    display: flex;
+    padding: 0 7%;
+  }
+}
+
+.form-wrapper {
+  max-width: 760px;
+  margin: 20px auto 0;
+
+  button[type=submit] {
+    margin: 30px 0 30px;
+  }
+
+  label {
+    font-size: 20px;
+  }
+
+  input,
+  button,
+  .button {
+    font-size: 18px;
+  }
+}
+
+.help {
+  color: $color-medium-gray;
+  font-style: italic;
+  font-size: 17px;
+
+  @include tablet {
+    text-align: left;
+  }
+}
+
+.pad-more {
+  margin-top: 20px;
+}
+
+.label {
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+.is-padded-top {
+  margin-top: 1em;
+}
+.is-pulled-left {
+  text-align: left;
+}
+
+.bubbles-wrapper {
+  @include tablet {
+    padding: 0 2%;
+  }
+}
+
+
+.input-wrapper .input-icon {
+  @include mobile {
+    top: 11px;
+  }
+  top: 5px;
+}
+
+.radio + .radio {
+  margin-left: 0;
+}
+</style>
