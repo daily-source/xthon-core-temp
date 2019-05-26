ccbfb6753dde2c1e00f78b9d907c37521a9804ac
John Vee Uminga <johnveeuminga@example.com>
2019-05-26 16:15:08 +0800

    Updated AppHeader.

---
+++ lib/components/Loseathon/AppBanner.2.vue
@@ -0,0 +1,36 @@
+<template>
+<div class='banner'>
+  <img src='@/assets/img/loseathon-banner.jpg' alt=''>
+  <div class="banner__contents">
+    <h2 class='has-text-weight-bold'>Lose Weight and Change the World</h2>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'AppBanner',
+}
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  height: 550px;
+  background-color: #fff;
+  position: relative;
+  
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    object-position: right;
+  }
+
+  &__contents {
+    position: absolute;
+    top: 45%;
+    left: 10%;
+    max-width: 400px;
+  }
+}
+</style>
