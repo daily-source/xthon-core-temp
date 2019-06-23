3a2787efbfa481ff41d87b927bffde9932276e4f
John Vee Uminga <johnveeuminga@example.com>
2019-06-23 13:42:03 +0800

    General template changes.

---
+++ lib/components/transitions/SlideFade.vue
@@ -0,0 +1,17 @@
+<template>
+<transition
+  name='slide-fade'
+>
+  <slot></slot>
+</transition>
+</template>
+
+<script>
+export default {
+  name: 'SlideFade'
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
