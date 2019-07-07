ac35da90098ff2b7110d6b95d2dc4ea108404f0c
Camilo Delvasto <camilodelvasto@gmail.com>
2019-07-07 16:50:09 -0500

    Change the modal for a user dialog component

--- lib/components/general/ShareByEmail.vue
+++ lib/components/general/ShareByEmail.vue
@@ -1,74 +1,84 @@
 <template>
   <div class="sharing-icons-row__wrapper">
-    <Modal
+    <UserDialog
       class="donate-error-modal"
       :prevent-body-scroll="false"
       :state="state"
+      :spinner="spinner"
       v-on:modal:close="dismiss()"
     >
       <div slot="header">
         <span v-if="!loggedIn">You need to be logged in</span>
-        <span v-if="loggedIn">Share this with a friend</span>
+        <span v-if="loggedIn">{{dialogHeader || 'Share this with a friend'}}</span>
       </div>
       <div slot="content">
-        <div v-if="!loggedIn">
-          <p>In order to share this page to a friend by email, you need to be logged in. We will include your first and last name with the email.</p>
-          <p class="is-centered dismiss-cta" @click="logIn()">
-            <span class="button is-centered is-large is-warning">Log in</span>
-          </p>
-        </div>
-        <div v-if="loggedIn">
-          <div class="columns form-column__wrapper form-column__extra-padded input-line is-multiline">
-            <div class="column is-5 form-column__label-column input-label">
-              <label :for="`input-name-first_${_uid}`" class="label">Friend's name:</label>
-            </div>
-            <div class="column is-5 form-column__input-column">
-              <div class="control input-wrapper">
-                <Icons icon="user" class="input-icon" iconwidth="20px" iconheight="20px" color="#999"></Icons>
-                <input class="input"
-                  type="text"
-                  placeholder=""
-                  :required="true"
-                  v-model="form.firstname"
-                  autocomplete="given-name"
-                  :id="`input-name-first_${_uid}`"
-                  v-on:input="$emit('input:first', $event.target.value)"
-                />
+        <div v-if="!sending">
+          <div v-if="!loggedIn">
+            <p>In order to share this page to a friend by email, you need to be logged in. We will include your first and last name with the email.</p>
+            <p class="is-centered dismiss-cta" @click="logIn()">
+              <span class="button is-centered is-large is-warning">Log in</span>
+            </p>
+          </div>
+          <div v-if="loggedIn">
+            <div class="columns form-column__wrapper form-column__extra-padded input-line is-multiline">
+              <div class="column is-5 form-column__label-column input-label">
+                <label :for="`input-name-first_${_uid}`" class="label">Friend's name:</label>
+              </div>
+              <div class="column is-7 form-column__input-column">
+                <div class="control input-wrapper">
+                  <Icons icon="user" class="input-icon" iconwidth="20px" iconheight="20px" color="#999"></Icons>
+                  <input class="input"
+                    type="text"
+                    placeholder=""
+                    :required="true"
+                    v-model="form.firstname"
+                    autocomplete="given-name"
+                    :id="`input-name-first_${_uid}`"
+                    v-on:input="$emit('input:first', $event.target.value)"
+                  />
+                </div>
+              </div>
+              <div class="column is-7 is-offset-5" v-if="firstnameErrorMessage">
+                <span class="error-message">{{firstnameErrorMessage}}</span>
               </div>
             </div>
-            <div class="column is-5 is-offset-5" v-if="firstnameErrorMessage">
-              <span class="error-message">{{firstnameErrorMessage}}</span>
-            </div>
-          </div>
-          <div class="columns form-column__wrapper form-column__extra-padded input-line is-multiline">
-            <div class="column is-5 form-column__label-column input-label">
-              <label :for="`input-email_${_uid}`" class="label">Friend's email:</label>
-            </div>
-            <div class="column is-5 form-column__input-column">
-              <div class="control input-wrapper">
-                <Icons icon="email" class="input-icon" iconwidth="20px" iconheight="20px" color="#999"></Icons>
-                <input class="input"
-                  placeholder=""
-                  :required="true"
-                  v-model="form.email"
-                  type="email"
-                  autocomplete="email"
-                  :id="`input-email_${_uid}`"
-                  v-on:input="$emit('input:first', $event.target.value)"
-                />
+            <div class="columns form-column__wrapper form-column__extra-padded input-line is-multiline">
+              <div class="column is-5 form-column__label-column input-label">
+                <label :for="`input-email_${_uid}`" class="label">Friend's email:</label>
+              </div>
+              <div class="column is-7 form-column__input-column">
+                <div class="control input-wrapper">
+                  <Icons icon="email" class="input-icon" iconwidth="20px" iconheight="20px" color="#999"></Icons>
+                  <input class="input"
+                    placeholder=""
+                    :required="true"
+                    v-model="form.email"
+                    type="email"
+                    autocomplete="email"
+                    :id="`input-email_${_uid}`"
+                    v-on:input="$emit('input:first', $event.target.value)"
+                  />
+                </div>
+              </div>
+              <div class="column is-7 is-offset-5" v-if="emailErrorMessage">
+                <span class="error-message">{{emailErrorMessage}}</span>
               </div>
             </div>
-            <div class="column is-5 is-offset-5" v-if="emailErrorMessage">
-              <span class="error-message">{{emailErrorMessage}}</span>
-            </div>
-          </div>
 
-          <p class="is-centered dismiss-cta" @click="share()">
-            <button class="button is-centered is-large is-danger" :disabled="!validateAllFields()">Share</button>
-          </p>
+            <p class="is-centered dismiss-cta">
+              <button
+                class="button is-centered is-large is-danger"
+                :disabled="!validateAllFields()"
+                @click="share()"
+              >Share</button>
+            </p>
+          </div>
+        </div>
+        <div v-if="sending">
+          <p>{{dialogMessage}}</p>
         </div>
       </div>
-    </Modal>
+    </UserDialog>
   </div>
 </template>
 
@@ -83,13 +93,17 @@ export default {
     return {
       form: {},
       firstnameErrorMessage: '',
-      emailErrorMessage: ''
+      emailErrorMessage: '',
+      spinner: false,
+      sending: false,
+      dialogMessage: '',
+      dialogHeader: ''
     }
   },
 
   components: {
     Icons,
-    Modal: () => import("Components/general/Modal.vue")
+    UserDialog: () => import("Components/general/UserDialog.vue")
   },
 
   computed: {
@@ -120,6 +134,10 @@ export default {
       return validName
     },
     dismiss () {
+      this.sending = false
+      this.dialogMessage = ''
+      this.dialogHeader = ''
+      this.form = {}
       this.$emit('dismiss')
     },
     logIn () {
@@ -129,6 +147,11 @@ export default {
     share () {
       // This won't use the sharer utility but will hit the actions directly.
       var shareUrl = `${window.location.origin}${window.location.pathname}`
+      // use the modal to give feedback when this starts
+      this.spinner = true
+      this.sending = true
+      this.dialogMessage = ''
+      this.dialogHeader = ''
       this.$store.dispatch('SHARE_BY_EMAIL', {
         url: shareUrl,
         via: process.env.SITE_NAME,
@@ -137,10 +160,14 @@ export default {
         token: this.$store.state.user.tokenData.accessToken
       })
       .then(response => {
-        console.log('success: ', response)
+        this.spinner = false
+        this.dialogMessage = `We have shared this site with your friend!`
+        this.dialogHeader = 'Success'
       })
       .catch(err => {
-        console.log('error: ', err)
+        this.spinner = false
+        this.dialogMessage = `The email could not be sent. ${err}`
+        this.dialogHeader = 'Error'
       })
     }
   },
