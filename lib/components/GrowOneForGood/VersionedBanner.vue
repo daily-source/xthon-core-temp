9282c68756a2468ad7dbf3a6859b517c68cc6e16
John Vee Uminga <johnveeuminga@example.com>
2019-07-12 03:47:21 +0800

    Sample demo for VersionSwitcher use cases.

---
+++ lib/components/GrowOneForGood/VersionedBanner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-banner">
+    <app-banner 
+      v-if='$version == 1'
+    />
+    <dynamic-banner 
+      v-else-if='$version == 2'
+    />
+  </div>
+</template>
+
+<script>
+import AppBanner from 'Components/GrowOneForGood/AppBanner'
+
+export default {
+  name: 'VersionedBanner',
+
+  components: {
+    AppBanner,
+  },
+}
+</script>
